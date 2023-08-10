35f4014f3e582b6c6466bf60ee81e7268f0aef17
Joe Park <jpark@outcomemd.com>
2023-08-10 12:31:01 -0700

    exercise 1.5

--- part1/courseInfo/src/App.js
+++ part1/courseInfo/src/App.js
@@ -3,18 +3,20 @@ import Content from './components/Content';
 import Total from './components/Total';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { part: 'Fundamentals of React', exercises: 10 },
-    { part: 'Using props to pass data', exercises: 7 },
-    { part: 'State of a component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { part: 'Fundamentals of React', exercises: 10 },
+      { part: 'Using props to pass data', exercises: 7 },
+      { part: 'State of a component', exercises: 14 },
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
