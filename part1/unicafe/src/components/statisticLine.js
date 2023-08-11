6e1a4a701b40481350bebd94e0716600d6ac6c46
Joe Park <jpark@outcomemd.com>
2023-08-11 16:35:13 -0700

    exercise 1.10 implement StatisticLine

---
+++ part1/unicafe/src/components/statisticLine.js
@@ -0,0 +1,10 @@
+const StatisticLine = ({text, value}) => {
+    return (
+      <>
+      {text} {value} <br />
+      </>
+    )
+  }
+  
+  export default StatisticLine;
+  
--- part1/unicafe/src/components/statistics.js
+++ part1/unicafe/src/components/statistics.js
@@ -2,6 +2,7 @@ import { REVIEWS } from "./button";
 import Total from "./total";
 import Average from './average';
 import Positive from './positive';
+import StatisticLine from "./statisticLine";
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
@@ -11,13 +12,13 @@ const Statistics = ({ good, neutral, bad }) => {
     No feedback given
     </>
   );
-  
+
   return (
     <>
       <h1>statistics</h1>
-      {REVIEWS.GOOD} {good}<br/>
-      {REVIEWS.NEUTRAL} {neutral}<br/>
-      {REVIEWS.BAD} {bad}< br/>
+      <StatisticLine text={REVIEWS.GOOD} value={good} />
+      <StatisticLine text={REVIEWS.NEUTRAL} value={neutral} />
+      <StatisticLine text={REVIEWS.BAD} value={bad} />
       <Total good={good} neutral={neutral} bad={bad} />
       <Average good={good} neutral={neutral} bad={bad} />
       <Positive good={good} neutral={neutral} bad={bad} />
