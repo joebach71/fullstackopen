00f3e817014f29e3f6ad07af64206e7b7faa9e18
Joe Park <jpark@outcomemd.com>
2023-08-10 22:26:44 -0700

    exercise 1.9: no feedback

--- part1/unicafe/src/components/statistics.js
+++ part1/unicafe/src/components/statistics.js
@@ -4,6 +4,14 @@ import Average from './average';
 import Positive from './positive';
 
 const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  if (!total) return (
+    <>
+    <h1>statistics</h1>
+    No feedback given
+    </>
+  );
+  
   return (
     <>
       <h1>statistics</h1>
